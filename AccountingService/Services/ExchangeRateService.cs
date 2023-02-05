fb4e5205718ae69a74334714225846d05b36767b
berda97 <lazarberdicberda@gmail.com>
2023-02-05 22:06:38 +0100

    exchange rate once per day

--- AccountingService/Services/ExchangeRateService.cs
+++ AccountingService/Services/ExchangeRateService.cs
@@ -2,42 +2,37 @@
 using RestSharp;
 
 namespace AccountingService.Services
+     
 {
     public class ExchangeRateService
     {
         private static JToken _exchangeRates;
-
+        private static DateTime _lastUpdate;
+        private static readonly TimeSpan UpdateInterval = TimeSpan.FromDays(1);
         public double GetCurrencyExchangeRate(string currency)
         {
-            if (currency == null)
+            if (currency == null )
             {
                 return 1;
             }
-
             ReloadExchangeRates();
-
-            return _exchangeRates[currency].Value<double>();
+            return _exchangeRates[currency].Value<double>();       
         }
-
         private static void ReloadExchangeRates()
         {
-            if(_exchangeRates != null )
+            if (_exchangeRates != null && (DateTime.Now - _lastUpdate) < UpdateInterval)
             {
                 return;
             }
-
             var client = new RestClient("https://api.apilayer.com/fixer/latest");
-
             var request = new RestRequest();
             request.Method = Method.Get;
-            request.AddHeader("apikey", "[API-KEY]");
-
+            request.AddHeader("apikey", "6IRdNpTdHNvBw5bd4ouVVoAgo5WpZA2");
             RestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
-
             var obj = JObject.Parse(response.Content);
             _exchangeRates = obj["rates"];
-           
+            _lastUpdate= DateTime.Now;  
         }
     }
 }
